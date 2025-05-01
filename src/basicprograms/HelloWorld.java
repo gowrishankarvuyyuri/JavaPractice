6fa0253ab094d2c776c725031507d65d9ef5e091
gowrishankarvuyyuri <gowrishankarvuyyuri@outlook.com>
2025-05-01 20:31:36 +0530

    added prime number program

--- src/basicprograms/HelloWorld.java
+++ src/basicprograms/HelloWorld.java
@@ -1,11 +1,20 @@
 package basicprograms;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!,"+" I am Gowri Shankar Vuyyuri"+"\t");
-		System.out.println("\t"+"Prem is my Nephew");
-	} 
 
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!," + " I am Gowri Shankar Vuyyuri" + "\t");
+		System.out.println("\t" + "Prem is my Nephew");
+		// Even or Odd Number
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a number : ");
+		int number = scanner.nextInt();
+		String result = (number % 2 == 0) ? "Even" : "Odd";
+		scanner.close();
+		System.out.println("The number is : " + result);
+	}
 }
---
+++ src/basicprograms/Pattern.java
@@ -0,0 +1,19 @@
+package basicprograms;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int input = 6;
+		boolean prime = true;
+		
+		for (int i = 2; i < input/2; i++) {
+			if (input % i == 0) {
+				prime = false;
+				break;
+			}
+		}
+		System.out.println("Number "+input+ " is "+ (prime? "Prime Number" : "Non-Prime Number"));
+	}
+}
