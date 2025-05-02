7e1007a721e6cf0a67cdb64807ad184a69bd715f
gowrishankarvuyyuri <gowrishankarvuyyuri@outlook.com>
2025-05-02 20:19:29 +0530

    Added my Name to Patterns

--- src/basicprograms/PrimeNumber.java
+++ src/basicprograms/PrimeNumber.java
@@ -93,5 +93,72 @@ public class PrimeNumber {
 			}
 			System.out.println();
 		}
+		System.out.println();
+		
+		
+		// Printing My Name
+		for (int row = 1; row <= input; row++) {
+			for (int col = 1; col <= input; col++) {
+				if(row == 1 || col == 1 || row == input || (row == 3 && col >= 3) || (row == 4 && col == 5))
+					System.out.print(" * ");
+				else
+					System.out.print("   ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		
+		for (int row = 1; row <= input; row++) {
+			for (int col = 1; col <= input; col++) {
+				if (row == 1  || col == 1 || row == 5 || col == 5)
+					System.out.print(" * ");
+				else
+					System.out.print("   ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		
+		for (int row = 1; row <= input; row++) {
+			for (int col = 1; col <= input; col++) {
+				if (col == 1 || col == 5 || (row == 3 && col == 3))
+					System.out.print(" * ");
+				else if (row == 4) {
+					if (col == input - row + 1 || col == input - 1)
+						System.out.print(" * ");
+					if (col == 3)
+						System.out.print("   ");
+				}
+				else
+					System.out.print("   ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		for (int row = 1; row <= input; row++) {
+			for (int col = 1; col <= input - 2; col++) {
+				if ((row == 1 || row == 3) || col == 1 || (col == 3 && row != 4) || (row == 4 && col == 2))
+					System.out.print(" * ");
+				else 
+					System.out.print("   ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		
+		for (int row = 1; row <= input; row++) {
+			for (int col = 1; col <= input; col++) {
+				if (row == 1 || row == 5 || col == 3)
+					System.out.print(" * ");
+				else 
+					System.out.print("   ");
+			}
+			System.out.println();
+		}
+		System.out.println();
 	}
 }
